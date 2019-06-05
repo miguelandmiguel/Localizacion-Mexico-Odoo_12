f849bb2d6097de096e3c9ce8fba937de5e5593bf
Miguel Miguel <miguel.miguel@bias.com.mx>
2019-06-05 15:32:56 -0500

    Modulo Nominas
    [FIX] -- HR Registro Patronal

--- l10n_mx_payroll/models/res_company.py
+++ l10n_mx_payroll/models/res_company.py
@@ -7,6 +7,7 @@ class ResCompany(models.Model):
     
     # cfdi_curp = fields.Char(string="CURP", oldname="curp", help="Llenar en caso de que el empleador sea una persona física")
     # cfdi_registropatronal_id = fields.Many2one('l10n_mx_payroll.regpat', string='Registro patronal', oldname='registro_patronal_id')
-    cfdi_registropatronal_id = fields.Char(string="Registro patronal", help="")
+    # cfdi_registropatronal_id = fields.Char(string="Registro patronal", help="")
     cfdi_riesgo_puesto_id = fields.Many2one("l10n_mx_payroll.riesgo_puesto", string="Clase riesgo", oldname="riesgo_puesto_id")
+    cfdi_registropatronal_id = fields.Many2one('l10n_mx_payroll.regpat', string='Registro patronal', oldname='registro_patronal_id')
     
