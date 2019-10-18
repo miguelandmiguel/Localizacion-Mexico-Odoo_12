454bc868bf640d4b75e516a40c17cf9f5b417f93
Miguel Miguel <miguel.miguel@bias.com.mx>
2019-10-18 17:47:47 -0500

    Modulo CFDI
    [FIX] -- Validador XML CFDI

--- bias_base_report/models/res_company.py
+++ bias_base_report/models/res_company.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import base64
+from lxml import etree
 import logging
 import json
+from json import dumps, loads
 import uuid
 import requests
+from suds.client import Client
+from xmljson import badgerfish as bf
 
+from itertools import groupby
 import odoo
 from odoo import api, fields, models, tools, exceptions, _
 
@@ -83,3 +89,60 @@ class company(models.Model):
         if res_datas.get('error'):
             return {'error': '%s'%res_datas['error']}
         return res_datas
+
+
+
+
+
+
+    @api.multi
+    def cfdi_call_service(self, service_type=False, xml64=False):
+        for company_id in self:
+            pac_name = company_id.l10n_mx_edi_pac
+            if not pac_name:
+                continue
+            pac_service_func = 'cfdi_call_service_%s_%s'%(pac_name, service_type)
+            test = company_id.l10n_mx_edi_pac_test_env
+            username = company_id.l10n_mx_edi_pac_username
+            password = company_id.l10n_mx_edi_pac_password
+            if test:
+                url = 'http://demo-facturacion.finkok.com/servicios/soap/%s.wsdl'%(service_type)
+            else:
+                url = 'http://facturacion.finkok.com/servicios/soap/%s.wsdl'%(service_type)
+            pac_info = getattr(self, pac_service_func)(test=test, url=url, username=username, password=password, xml64=xml64)
+            return pac_info
+
+    def cfdi_call_service_finkok_validation(self, test=False, url=False, username=False, password=False, xml64=False):
+        client = Client(url, cache=None, timeout=80)
+        contenido = client.service.validate(xml64.decode('utf-8'), username, password)
+        try:
+            try:
+                error = contenido.error
+                return {u'error': {u'message': "Error validar XML \n\n %s "%( error.upper() )}}
+            except Exception as e:
+                if not contenido.sat:
+                    return {u'error': {u'message': "No se pudo establecer comunicacion con el SAT. \n Favor de intentar nuevamente."  }}
+                return {
+                    "xml_valido": contenido.xml,
+                    "sello_valido": contenido.sello,
+                    "sello_sat_valido": contenido.sello_sat,
+                    "estado": str(contenido.sat and contenido.sat.Estado or ''),
+                    "cod_estatus": str(contenido.sat and contenido.sat.CodigoEstatus or ''),
+                    "xml_datas": self.cfdi_call_service_xml_datas(xml64)
+                }
+        except Exception as e:
+            return {u'error': {u'message': str(e)  }}
+        return ""
+
+    def cfdi_call_service_xml_datas(self, xml64):
+        datas = {}
+        xml = base64.b64decode(xml64).decode('utf-8')
+        registrofiscal = xml.find('registrofiscal:CFDIRegistroFiscal xmlns:schemaLocation="http://www.sat.gob.mx/registrofiscal http://www.sat.gob.mx/sitio_internet/cfd/cfdiregistrofiscal/cfdiregistrofiscal.xsd"')
+        if registrofiscal > 1:
+            xml = xml.replace('registrofiscal:CFDIRegistroFiscal xmlns:schemaLocation="http://www.sat.gob.mx/registrofiscal http://www.sat.gob.mx/sitio_internet/cfd/cfdiregistrofiscal/cfdiregistrofiscal.xsd"', 'registrofiscal:CFDIRegistroFiscal ')
+        registrofiscal = xml.find("registrofiscal:CFDIRegistroFiscal")
+        schemaLocation = xml.find('xsi:schemaLocation="http://www.sat.gob.mx/cfd/3 http://www.sat.gob.mx/sitio_internet/cfd/3/cfdv33.xsd"')
+        if registrofiscal > 1 and schemaLocation > 1:
+            xml = xml.replace('xsi:schemaLocation="http://www.sat.gob.mx/cfd/3 http://www.sat.gob.mx/sitio_internet/cfd/3/cfdv33.xsd"', 'xsi:schemaLocation="http://www.sat.gob.mx/cfd/3 http://www.sat.gob.mx/sitio_internet/cfd/3/cfdv32.xsd http://www.sat.gob.mx/registrofiscal http://www.sat.gob.mx/sitio_internet/cfd/cfdiregistrofiscal/cfdiregistrofiscal.xsd"')
+        datas = dumps(bf.data(etree.fromstring(xml)))
+        return datas
