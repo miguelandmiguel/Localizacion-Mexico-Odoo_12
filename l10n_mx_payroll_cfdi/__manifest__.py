60a8d908da9a8a57cbeac53bcdcfdfdfb873dbce
Miguel Miguel <migueldguezmiguel>
2022-11-29 18:16:12 -0600

    [FIX] Se corrige Layout BanBajio. Fecha de Aplicacion

--- l10n_mx_payroll_cfdi/__manifest__.py
+++ l10n_mx_payroll_cfdi/__manifest__.py
@@ -10,7 +10,7 @@
     'author': "OpenBias",
     'website': "https://bias.com.mx",
     'category': 'Uncategorized',
-    'version': '1.23',
+    'version': '1.24',
     'depends': ['base_setup', 'hr', 'hr_payroll', 'account', 'l10n_mx_payroll'],
     'data': [
         'views/report_templates.xml',
